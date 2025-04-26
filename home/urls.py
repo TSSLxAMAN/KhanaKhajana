723813157fb1ccf50b92ed7a137cf159ba3ab7e2
TSSLXAMAN <amancse@student.iul.ac.in>
2025-04-27 02:14:20 +0530

    Frontend Completed

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from home.views import *
+from .views import *
 
 urlpatterns = [
     path('', homepage, name='home'), 
+    path('cuisine/', cuisine, name='cuisine'), 
+    path('contact/', contact, name='contact'), 
+    path('aboutus/', aboutus, name='aboutus'), 
+    path('login/', login, name='login'), 
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, 'home/home.html')
+
+def cuisine(request):
+    return render(request, 'home/cuisine.html')
+
+def aboutus(request):
+    return render(request, 'home/aboutus.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
+def login(request):
+    return render(request, 'home/login.html')
